06122156fb0dbf09a916009c8885d99981ad81fe
Peter Vierstrom <peter4strom@ymail.com>
2016-01-13 21:33:45 +0100

    silent curl lacks feedback

--- install_js.sh
+++ install_js.sh
@@ -5,7 +5,7 @@ version=5.1.0
 dstdir=/opt/nodejs-$version
 
 mkdir -p $dstdir
-curl -sSL "https://nodejs.org/dist/v${version}/node-v${version}-linux-x64.tar.gz" \
+curl -L "https://nodejs.org/dist/v${version}/node-v${version}-linux-x64.tar.gz" \
 	| tar xzC $dstdir --strip-components=1
 
 ln -sf $dstdir/bin/node /usr/local/bin/node
